07ca2b3199b5ecaec1b5e5a6bec93b59b14c1ddc
Cher Wei Soh <cwsoh@zoecity.com>
2009-07-02 17:29:18 +0800

    Add lua recipe in tokyo cookbook

---
+++ cookbooks/tokyo/attributes/lua.rb
@@ -0,0 +1,2 @@
+tokyo_lua Mash.new unless attribute?("tokyo_lua")
+tokyo_lua[:url] = "http://www.lua.org/ftp/lua-5.1.4.tar.gz"
---
+++ cookbooks/tokyo/recipes/lua.rb
@@ -0,0 +1,17 @@
+url = node[:tokyo_lua][:url]
+tarball = url.split('/').last
+name = tarball.split('.tar.gz').first
+
+bash "install_lua" do
+user "root"
+  cwd "/tmp"
+  code <<-EOH 
+  wget #{url}
+  tar -xvf #{tarball}
+  cd #{name}
+  ./configure
+  make
+  make install
+  EOH
+  not_if do File.exists?("/usr/local/share/tokyolua") end
+end
