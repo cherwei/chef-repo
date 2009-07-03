4eba272f67d69078cccf16423a100c38264881fb
Cher Wei Soh <cwsoh@zoecity.com>
2009-07-03 09:57:16 +0800

    Add actsasflinn-ruby-tokyotyrant in dna

--- cookbooks/tokyo/recipes/default.rb
+++ cookbooks/tokyo/recipes/default.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+# these libs are needed in order to install actsasflinn-ruby-tokyotyrant
 include_recipe "tokyo::cabinet"
 include_recipe "tokyo::lua"
 include_recipe "tokyo::tyrant"
